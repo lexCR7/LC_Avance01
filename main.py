f9ba01d807eede7a2c127aae35eec0364d65abee
lexCR7 <alexander.chamorro@unmsm.edu.pe>
2024-08-01 16:47:19 -0500

    parte logica

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from scanner import Scanner
-def main():
-    
-    s = Scanner(cadena) # Leer cadena
-    tokens = s.scanAll()
-    print(tokens)
-
-if __name__ == '__main__':
-    main()
+int dias = Integer.parseInt(txtDias.getText());
+        double km = Double.parseDouble(txtKm.getText());
+        
+        double tarifa = 0.0;
+        if(rbPequeño.isSelected()) tarifa = 15;
+        if(rbMediano.isSelected()) tarifa = 20;
+        if(rbGrande.isSelected()) tarifa = 30;
+        
+        double costo=0.0;
+        if(rbPequeño.isSelected()) tarifa = 0.2*km;
+        if(rbMediano.isSelected()) tarifa = 0.3*km;
+        if(rbGrande.isSelected()) tarifa = 0.4*km;
+        
+        double monto=(tarifa*dias)+costo;
+        
+        double aumento=0;
+        if(km>10){
+            aumento = monto*(25/100.0);
+        }
+        monto+=aumento;
+        
+        txtR.SetText("");
+        txtR.append("La cantidad de dias es: "+dias);
+        txtR.append("\nLa tarifa asignada es: $"
+                + String.format("%.2f", tarifa));
+        txtR.append("\nEl costo por kilometro es: $"
+                + String.format("%.2f", costo));
+        txtR.append("\nEl monto a pagar es: $"
+                + String.format("%.2f", monto));
